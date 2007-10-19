a58965b011cfa16c7b8aacd6d526af72559554ce
hustzxq524 <hustzxq524@dc7256ff-9735-0410-b5a2-c1270dee5d4c>
2007-10-19 09:39:50 +0000

    c

--- web-freecell/custom.js
+++ web-freecell/custom.js
@@ -3,10 +3,12 @@ $(function() {
 	$("#btn1").click(function() {
 		if(g_count <= 54) {
 		    card_fn = "cards/card_" + g_count + ".gif";
-		    //console.log("card_fn is %s", card_fn);
+		    /*
 		    img_html="<img src=\"" + card_fn + "\">";
 		    console.log("img is %s", img_html);
 		    $("#card_field").html(img_html);
+		    */
+		    $("#card_field").css('background-image','url('+card_fn+')');
 		    g_count++;
 		}
 	    });
