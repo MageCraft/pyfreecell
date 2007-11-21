9ad2f004b02f9e3aab6578abb73bd807b8b3cf50
hustzxq524 <hustzxq524@dc7256ff-9735-0410-b5a2-c1270dee5d4c>
2007-11-21 09:25:41 +0000

    add two functions to psp2d.Image

--- psp-russia/russia/script.py
+++ psp-russia/russia/script.py
@@ -1,7 +1,30 @@
 import psp2d
 import sys
+import pygame
+pygame.init()
+import sys
 
 font_path = 'images/font_arial_10_white.png'
+
+MAX_ROW = 10
+MAX_COL = 20
+
+def main_1():
+    scr = psp2d.Screen()
+    fnt = psp2d.Font(font_path)
+    img = psp2d.Image(480, 272)
+    img.clear(psp2d.Color(0, 0, 0))
+    img.drawText(0, 0, 'Hello, world', psp2d.Color(255,0,0))
+    #fnt.drawText(img, 0, 0, 'Hello, world')
+    #img.drawLine(100, 100, 150, 150, psp2d.Color(255,0,0))
+    pygame.draw.line(img.surface, (255,0,0,255), (100,100), (150,150), 2)
+    scr.blit(img)
+    scr.swap()
+
+    while True:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT: sys.exit()
+
 
 def main():
     scr = psp2d.Screen()
@@ -9,6 +32,7 @@ def main():
     img = psp2d.Image(480, 272)
     img.clear(psp2d.Color(0, 0, 0))
     fnt.drawText(img, 0, 0, 'Hello, world')
+    img.drawLine(100, 100, 150, 150, psp2d.Color(255,0,0))
     scr.blit(img)
     scr.swap()
     while True:
@@ -36,4 +60,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main_1()
