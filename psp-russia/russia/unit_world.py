0e89522e6e91f8facfbcce23cce240062da3f6c1
hustzxq524 <hustzxq524@dc7256ff-9735-0410-b5a2-c1270dee5d4c>
2007-11-27 02:20:40 +0000

    add

--- psp-russia/russia/unit_world.py
+++ psp-russia/russia/unit_world.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 EMPTY = -1
 GRAY  = -2
+
 
 class Unit: 
     def __init__(self, value=EMPTY):
@@ -12,23 +14,37 @@ class Pos:
     def __init__(self, x=Empty, y=Empty):
         self.x = x
         self.y = y
+
     def left(self, dx=1): return Pos(self.x-dx, self.y)
     def right(self, dx=1): return Pos(self.x+dx, self.y)
-    def up(self, dy=1): return Pos(self.x, self.y-dy)
-    def down(self, dy=1): return Pos(self.x, self.y+dy)
+    def above(self, dy=1): return Pos(self.x, self.y-dy)
+    def below(self, dy=1): return Pos(self.x, self.y+dy)
     def move(self,dx,dy):
         self.x += dx
         self.y += dy
         return self
 
-inline UnitWorld::UnitWorld(int _maxCol, int _maxRow, int _minCol, int _minRow)
-    : maxCol(_maxCol), maxRow(_maxRow), minCol(_minCol), minRow(0), fullLineCount(0)
-{
-    units = new Unit*[maxCol];
-    for( int col = 0 ; col < maxCol ; ++col )
-        units[col] = new Unit[maxRow];
-    clear();
-}
+class RS_Column:
+    def __init__(self, size, init):
+        self.init = init
+        self.data = [ self._get_init(i) for i in range(size)]
+
+    def _get_init(self, index):
+        if callable(self.init):
+            return self.init(index)
+        return self.init
+
+    def __getitem__(self, index):
+        return self.data[index]
+    def __setitem__(self, index, value):
+        self.data[index] = value
+    def __delitem__(self, index):
+        del self.data[index]
+        self.data.append(self._get_init(len(self.data)))
+    def __repr__(self):
+        return self.data.__repr__()
+    def __len__(self):
+        return self.data.__len__()
 
 class UnitWorld:
     def __init__(self, max_col, max_row, min_col=0, min_row=0):
@@ -38,11 +54,8 @@ class UnitWorld:
         self.max_row = max_row
 
         #matrix
-        for x in range(max_col):
-            col = []
-            for y in range(max_row):
-                col.append(Unit())
-            self.units.append(col)
+        for i in range(max_col):
+            self.units.append(RS_Column(self.max_row, Unit(EMPTY)))
 
     def init(self):
         pass
@@ -51,156 +64,61 @@ class UnitWorld:
         for col in self.units:
             for u in col:
                 u.value = EMPTY
-        self.full_lines.clear()
+
+    def dump(self):
+        for col in self.units:
+            func = lambda v: v!=EMPTY and "1" or "0"
+            print "\t".join(map(func, col))
+
+    def dump_with_color(self):
+        for col in self.units:
+            print "\t".join(map(str, col))
+
+    def __repr__(self):
+        self.dump()
 
     def set_empty(self, x,y): 
         self.units[x][y].value = EMPTY
-    def is_empty(self, x,y): return self.units[x][y].value == EMPTY
-    def value(self, x,y) return self.units[x][y].value
-    def set_value(self, x,y, value) self.units[x][y].value = value
-    def get_full_line_count(self): return len( filter(self.full_lines, lambda l: l!=EMPTY) )
+
+    def is_empty(self, x,y): 
+        return self.units[x][y].value == EMPTY
+
+    def value(self, x,y): 
+        return self.units[x][y].value
+
+    def set_value(self, x,y, value): 
+        self.units[x][y].value = value
+
+    def check_full_lines(self):
+        assert( self.full_lines.__len__() == 0 )
+        for r in range(self.max_row-1, -1, -1):
+            row = self.get_row(r)
+            count = len( filter(lambda v: v == EMPTY, row) )
+            if count == self.max_col: 
+                break;
+            if count == 0:
+                self.full_lines.append(r)
+
+    def get_row(self, row):
+        return map(lambda col: col[row], self.units)
+
+    def get_full_line_count(self): 
+        return len(self.full_lines)
+
+    def flag_full_lines(self):
+        for row in self.full_lines:
+            self._gray_line(row)
+
+    def remove_full_lines(self):
+        for l in self.full_lines:
+            self._remove_row(l)
+        self.full_lines = []
             
-        
+    #private
+    def _gray_line(self,row):
+        for col in self.units:
+            col[row] = GRAY
 
-
-{
-    public:
-        UnitWorld(int maxCol, int maxRow, int minCol=0, int minRow=0);
-        ~UnitWorld();
-
-    public:
-        void init();
-        void clear();
-
-        void setEmpty(int x, int y);
-        void setEmpty(const Pos& p);
-
-        bool isEmpty(int x, int y) const;
-        bool isEmpty(const Pos& p) const;
-
-        int  value(int x, int y) const;
-        int  value(const Pos& p) const;
-
-        void setValue(int x, int y, int val);
-        void setValue(const Pos& p, int val);
-
-        int  getMaxCol() const;
-        int  getMaxRow() const;
-
-        int  getFullLineCount();
-        void flagFullLines();
-        void removeFullLines();
-
-    protected:
-        const int maxCol;
-        const int maxRow;
-        const int minCol;
-        const int minRow;
-        Unit** units;
-        int fullLineCount;
-        int fullLines[4];
-
-    protected:
-        void setEmpty(int row);
-        void setGrayFlag(int row);
-};
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// implement of Pos
-//
-inline Pos::Pos()
-{ x = EMPTY; y = EMPTY; } 
-
-inline Pos::Pos(int _x, int _y) 
-{ x = _x ; y = _y ; } 
-
-inline Pos& Pos::operator=(const Pos& p)
-{ 
-    if( this == &p )
-        return *this; 
-    x = p.x; y = p.y; 
-    return *this; 
-}
-
-inline Pos::Pos(const Pos& p)
-{ *this = p; }
-
-
-/////////////////////////////////////////////////////////////////////////////////////////
-// implement of UnitWorld
-//
-inline UnitWorld::UnitWorld(int _maxCol, int _maxRow, int _minCol, int _minRow)
-    : maxCol(_maxCol), maxRow(_maxRow), minCol(_minCol), minRow(0), fullLineCount(0)
-{
-    units = new Unit*[maxCol];
-    for( int col = 0 ; col < maxCol ; ++col )
-        units[col] = new Unit[maxRow];
-    clear();
-}
-
-inline void  UnitWorld::init() 
-{
-
-}
-inline void UnitWorld::clear() 
-{
-    for( int col = 0 ; col < maxCol ; ++col )
-        for( int row = 0 ; row < maxRow ; ++row )
-            units[col][row].value = EMPTY;
-    for( int i = 0 ; i < 4 ; ++i )
-        fullLines[i] = EMPTY;
-    fullLineCount = 0;
-}
-inline void UnitWorld::setEmpty(int x, int y )
-{ 
-    assert(x >= 0 && x < maxCol); 
-    assert(y >= 0 && y < maxRow); 
-    units[x][y].value = EMPTY; 
-}
-
-inline void UnitWorld::setEmpty(const Pos& p)
-{ 
-    setEmpty(p.x, p.y); 
-}
-
-inline bool UnitWorld::isEmpty(int x, int y) const 
-{ 
-    if( x < minCol || x > maxCol-1 || y < 0 || y > maxRow-1 )
-        return false;
-    return ( units[x][y].value == EMPTY );
-}
-
-inline bool UnitWorld::isEmpty(const Pos& p) const 
-{ 
-    return isEmpty(p.x,p.y); 
-}
-
-inline int UnitWorld::value(int x, int y) const 
-{ 
-    return units[x][y].value; 
-}
-
-inline int UnitWorld::value(const Pos& p) const
-{ 
-    return value(p.x,p.y); 
-}
-
-inline void UnitWorld::setValue(int x, int y, int val) 
-{ 
-    assert(x >= 0 && x < maxCol); 
-    assert(y >= 0 && y < maxRow); 
-    units[x][y].value = val; 
-}
-
-inline void UnitWorld::setValue(const Pos& p, int val) 
-{ 
-    setValue(p.x,p.y,val); 
-}
-
-inline int UnitWorld::getMaxCol() const 
-{ return maxCol; }
-
-inline int UnitWorld::getMaxRow() const 
-{ return maxRow; }
-
-
+    def _remove_row(self, row):
+        for col in self.units:
+            del col[row]
--- psp-russia/russia/util.py
+++ psp-russia/russia/util.py
@@ -1,29 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from itertools import imap
+DEBUG = True
 
-
-def Matrix(col, row, initial=None):
-    matrix = []
-    for x in range(col): 
-        col = []
-        for y in range(row):
-            if callable(initial): 
-                col.append(initial(x,y))
-            else:
-                col.append(initial)
-        matrix.append(col)
-    return matrix
-
-def dump_matrix(matrix):
-    for col in matrix:
-        print "\t".join(imap(str, col))
-
-
-if __name__ == '__main__':
-    matrix1 = Matrix(10,20, lambda x,y: x+y)
-    matrix2 = Matrix(20,10, None)
-    for m in (matrix1, matrix2):
-        dump_matrix(m)
-
+def log(*arg_list):
+    if DEBUG:
+        print " ".join(map(str, arg_list))
