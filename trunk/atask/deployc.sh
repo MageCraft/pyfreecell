74d83fb0fca0b4f928460c8003d394a0ef89a834
hustzxq524 <hustzxq524@dc7256ff-9735-0410-b5a2-c1270dee5d4c>
2009-02-09 14:27:58 +0000

    add script to generate air package and copy to web dir to public update the air package

--- trunk/atask/deployc.sh
+++ trunk/atask/deployc.sh
@@ -1,16 +1,15 @@
 sdk_path="C:/Program Files/Adobe/Flex Builder 3/sdks/3.1.0"
-adt="java -jar -Xmx512m ${sdk_path}/lib/adt.jar"
-adt="C:/Program Files/Adobe/Flex Builder 3/sdks/3.1.0/bin/adt"
 storetype=pkcs12
 keystore=xatasks.p12
 storepass=780524
 app_path=atasks/bin-debug/
 app_xml=${app_path}/atasks-app.xml
+swf=atasks.swf
+air_package=xatasks.air
 
-function run_adt
-{
-"$adt" -package -storetype $storetype -keystore $keystore -storepass $storepass \
-xatasks.air $app_xml -C $app_path 
-}
+echo "generating $air_package"
+java -jar -Xmx512m "${sdk_path}/lib/adt.jar" -package -storetype $storetype -keystore $keystore -storepass $storepass $air_package $app_xml -C $app_path $swf
+echo 'done'
 
-run_adt
+web_dir=atasksd/media
+mv -f $air_package $web_dir
