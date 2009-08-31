c0f527e3755cec775d1f1335a9dbdd380a53d5ac
hustzxq524 <hustzxq524@dc7256ff-9735-0410-b5a2-c1270dee5d4c>
2009-08-31 09:28:06 +0000

    add a new flex project flatasks that running in the browser

--- atask/atasksd/atasks.py
+++ atask/atasksd/atasks.py
@@ -22,6 +22,13 @@ client_name='xatasks'
 client_versions_file='version.xml'
 client_releases_dir='releases'
 
+import os
+from google.appengine.ext.webapp import template
+
+
+templates_dir = os.path.join(os.path.dirname(__file__), 'templates')
+
+
 def current_client_versions():
     f=open(client_versions_file)
     dom=parse(f)
@@ -39,6 +46,11 @@ def current_client():
     client_package_release_note=release_dir + '/' + 'release_note.txt'
     return client_package_name, client_package_url, client_package_release_note
 
+
+class MyRequseHandler(webapp.RequestHandler):
+    def render(self, template_file, template_vars):
+        path = os.path.join(templates_dir, template_file)
+        self.response.out.write(template.render(path, template_vars))
 
 
 class CheckUpdate(webapp.RequestHandler):
@@ -62,34 +74,45 @@ class Task(db.Model):
     email=db.StringProperty()
 
 
-class List(webapp.RequestHandler):
-    def get(self):
-       email = self.request.get('email').lower()       
-       trace(email)
-       tasks = Task.gql("WHERE email = :1 ORDER BY date", email)
-       doc = minidom.Document()
-       tasks_node = doc.createElement('tasks')
-       doc.appendChild(tasks_node)
-       for task in tasks:
-           task_node = doc.createElement('task')
-           task_node.setAttribute('key', str(task.key()))
-           task_node.setAttribute('content', task.content)
-           task_node.setAttribute('done', str(task.done).lower())
-           tasks_node.appendChild(task_node)
-       trace(doc.toxml())
-       self.response.headers['Content-Type'] = 'text/xml'
-       self.response.out.write(doc.toxml())
+class RESTHandler(webapp.RequestHandler):
+    def get_current_email(self):
+        user = users.get_current_user()
+        if user:
+            email = user.email().lower()
+        else:
+            email = self.request.get('email').lower()
+        return email
 
-class Add(webapp.RequestHandler):
+
+
+class List(RESTHandler):
+    def get(self):
+        email = self.get_current_email()
+        trace(email)
+        tasks = Task.gql("WHERE email = :1 ORDER BY date", email)
+        doc = minidom.Document()
+        tasks_node = doc.createElement('tasks')
+        doc.appendChild(tasks_node)
+        for task in tasks:
+            task_node = doc.createElement('task')
+            task_node.setAttribute('key', str(task.key()))
+            task_node.setAttribute('content', task.content)
+            task_node.setAttribute('done', str(task.done).lower())
+            tasks_node.appendChild(task_node)
+        trace(doc.toxml())
+        self.response.headers['Content-Type'] = 'text/xml'
+        self.response.out.write(doc.toxml())
+
+class Add(RESTHandler):
     def post(self):
         task = Task()
-        task.content = self.request.get('content');
-        task.email = self.request.get('email');
+        task.content = self.request.get('content')
+        task.email = self.get_current_email()
         task.put()
         self.response.out.write('key=%s' % (str(task.key())) )
 
 
-class Delete(webapp.RequestHandler):
+class Delete(RESTHandler):
     def post(self):
         key = self.request.get('key');
         trace(key);
@@ -98,7 +121,7 @@ class Delete(webapp.RequestHandler):
         task.delete()
         self.response.out.write('deleted')
         
-class Update(webapp.RequestHandler):
+class Update(RESTHandler):
     def post(self):
         key = self.request.get('key');
         content = self.request.get('content')
@@ -149,6 +172,26 @@ class MainPage(webapp.RequestHandler):
       </html>
       ''' % (curl, cname, crelnote, html1) 
       self.response.out.write(html)
+
+class Flatasks(MyRequseHandler):
+    def get(self):
+      user = users.get_current_user()
+      logout_url = users.create_logout_url(self.request.uri) 
+      login_url = users.create_login_url(self.request.uri)
+      template_vars = {
+              'user': user,
+              'login_url': login_url,
+              'logout_url': logout_url,
+              'js_dir' : 'media',
+              'media_dir' : 'media',
+              'swf': 'flatasks',
+              'app_name': 'flatasks',
+              }
+      self.render('flatasks.html', template_vars)
+
+        
+
+        
           
       
 application = webapp.WSGIApplication(
@@ -159,6 +202,7 @@ application = webapp.WSGIApplication(
                                       ('/delete', Delete),
                                       ('/update', Update),
                                       ('/checkupdate', CheckUpdate),
+                                      ('/flatasks', Flatasks),
                                      ],
                                      debug=True)
 
