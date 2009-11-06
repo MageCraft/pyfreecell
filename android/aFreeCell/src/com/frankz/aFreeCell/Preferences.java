458d5faaf5473a3f8a449dd2feaab42a6b911e3d
hustzxq524 <hustzxq524@dc7256ff-9735-0410-b5a2-c1270dee5d4c>
2009-11-06 03:54:44 +0000

    add game status only support portrait

--- android/aFreeCell/src/com/frankz/aFreeCell/Preferences.java
+++ android/aFreeCell/src/com/frankz/aFreeCell/Preferences.java
@@ -5,6 +5,8 @@ import android.preference.PreferenceActivity;
 
 public class Preferences extends PreferenceActivity {
 	public static final String KEY_ALERT_ILLEGAL_MOVE = "pref_key_alert_illegal_move";
+	public static final String KEY_DISPLAY_GAME_STATUS = "pref_key_display_game_status";
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {		
